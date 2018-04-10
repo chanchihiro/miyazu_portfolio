eaa2ba17ed21e40df20c7fbedc2a4ce3d9f28082
chihiro <chanchi0707@gmail.com>
2018-04-10 09:39:40 +0900

    worksの追加

--- dist/styles.a3b74baf.js
+++ dist/styles.a3b74baf.js
@@ -77,7 +77,7 @@ parcelRequire = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({28:[function(require,module,exports) {
+})({14:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -107,7 +107,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],27:[function(require,module,exports) {
+},{}],9:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -138,25 +138,25 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":28}],31:[function(require,module,exports) {
+},{"./bundle-url":14}],11:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":27}],32:[function(require,module,exports) {
+},{"_css_loader":9}],10:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":27}],12:[function(require,module,exports) {
+},{"_css_loader":9}],3:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./reset.css":31,"./variables.css":32,"./../images/slide1.png":90,"_css_loader":27}],139:[function(require,module,exports) {
+},{"./reset.css":11,"./variables.css":10,"./../images/slide1.png":12,"_css_loader":9}],95:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -186,7 +186,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '55047' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '65004' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -325,7 +325,7 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}],140:[function(require,module,exports) {
+},{}],96:[function(require,module,exports) {
 var getBundleURL = require('./bundle-url').getBundleURL;
 
 function loadBundlesLazy(bundles) {
@@ -406,7 +406,7 @@ LazyPromise.prototype.catch = function (onError) {
   if (this.promise === null) this.promise = new Promise(this.executor);
   return this.promise.catch(onError);
 };
-},{"./bundle-url":28}],0:[function(require,module,exports) {
-var b=require(140);b.load([["slide1.66ad2b13.png",90]]);
-},{}]},{},[139,0])
+},{"./bundle-url":14}],0:[function(require,module,exports) {
+var b=require(96);b.load([["slide1.66ad2b13.png",12]]);
+},{}]},{},[95,0])
 //# sourceMappingURL=/styles.a3b74baf.map
