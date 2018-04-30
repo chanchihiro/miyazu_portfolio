f4fc86dc5fe95f796046148be067ce56b458b7d7
chihiro <chanchi0707@gmail.com>
2018-04-30 16:52:20 +0900

    背景のためにthree.jsを追加

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
 /* import file */
+import * as THREE from 'three';
 
 /* main */
 $(document).ready(function(){
-  // slick.js
+  //// slick.js
   $('.miyazu-slides').slick({
   	autoplay: true,
   	draggable: true,
@@ -17,8 +18,41 @@ $(document).ready(function(){
   	nextArrow: '<button class="slide-arrow next-arrow"></button>',
   });
 
-  // banners
+  //// banners
   $('#miyazu-modal-button_close').on('click',function(){
   	$('#miyazu-modal-news').css('display','none');
   });
+
+  //// three.js
+  let scene;
+  let sphere;
+  let camera;
+  let renderer;
+  let light;
+  let width = 500;
+  let height = 250;
+
+  // scene ステージ
+  scene = new THREE.Scene();
+  // メッシュ
+  sphere = new THREE.Mesh(
+  	new THREE.SphereGeometry(100, 20, 20), // geometry 形状
+  	new THREE.MeshLambertMaterial({color: 0x8dc3ff}) // material 材質、色
+  );
+  sphere.position.set(0, 0, 0);
+  scene.add(sphere);
+  // ライト
+  light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(0, 100, 30);
+  scene.add(light);
+  // カメラ
+  camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+  camera.position.set(200, 100, 300);
+  camera.lookAt(scene.position);
+  // レンダラー
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(width, height);
+  renderer.setClearColor(0xeeeeee);
+  document.getElementById('stage').appendChild(renderer.domElement);
+  renderer.render(scene, camera);
 });
