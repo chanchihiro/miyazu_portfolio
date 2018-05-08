29b7e668823363f796918d5e136e53c842e6f9aa
chihiro <chanchi0707@gmail.com>
2018-05-08 09:20:17 +0900

    画面幅を変えた

--- dist/styles.a3b74baf.js
+++ dist/styles.a3b74baf.js
@@ -168,7 +168,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./reset.css":18,"./variables.css":21,"./lib/slick.css":22,"./lib/slick-theme.css":23,"./../images/arrow2.png":19,"./../images/arrow.png":20,"./../images/slide1.png":14,"_css_loader":16}],81:[function(require,module,exports) {
+},{"./reset.css":18,"./variables.css":21,"./lib/slick.css":22,"./lib/slick-theme.css":23,"./../images/arrow2.png":19,"./../images/arrow.png":20,"./../images/slide1.png":14,"_css_loader":16}],83:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -337,7 +337,7 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}],82:[function(require,module,exports) {
+},{}],84:[function(require,module,exports) {
 var getBundleURL = require('./bundle-url').getBundleURL;
 
 function loadBundlesLazy(bundles) {
@@ -419,6 +419,6 @@ LazyPromise.prototype.catch = function (onError) {
   return this.promise.catch(onError);
 };
 },{"./bundle-url":30}],0:[function(require,module,exports) {
-var b=require(82);b.load([["ajax-loader.5951cde3.gif",25],["slick.29d4c64f.eot",26],["slick.526dd301.woff",27],["slick.efb232c4.ttf",28],["slick.fcb6734e.svg",29],["arrow2.e54e5543.png",19],["arrow.2b8fda1f.png",20],["slide1.66ad2b13.png",14]]);
-},{}]},{},[81,0])
+var b=require(84);b.load([["ajax-loader.5951cde3.gif",25],["slick.29d4c64f.eot",26],["slick.526dd301.woff",27],["slick.efb232c4.ttf",28],["slick.fcb6734e.svg",29],["arrow2.e54e5543.png",19],["arrow.2b8fda1f.png",20],["slide1.66ad2b13.png",14]]);
+},{}]},{},[83,0])
 //# sourceMappingURL=/styles.a3b74baf.map
