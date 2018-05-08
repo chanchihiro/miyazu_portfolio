418fef9faceedbd1e46aaced4604d6a2770bd6af
chihiro <chanchi0707@gmail.com>
2018-05-09 01:28:58 +0900

    メニューの実装

--- dist/app.b6bd5372.js
+++ dist/app.b6bd5372.js
@@ -77,7 +77,7 @@ parcelRequire = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({24:[function(require,module,exports) {
+})({23:[function(require,module,exports) {
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -41346,7 +41346,7 @@ exports.Projector = Projector;
 exports.CanvasRenderer = CanvasRenderer;
 exports.SceneUtils = SceneUtils;
 exports.LensFlare = LensFlare;
-},{}],2:[function(require,module,exports) {
+},{}],4:[function(require,module,exports) {
 'use strict';
 
 var _three = require('three');
@@ -41409,7 +41409,7 @@ $(document).ready(function () {
   document.getElementById('stage').appendChild(renderer.domElement);
   renderer.render(scene, camera);
 }); /* import file */
-},{"three":24}],33:[function(require,module,exports) {
+},{"three":23}],33:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -41439,7 +41439,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '63320' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54047' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -41578,5 +41578,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[33,2])
+},{}]},{},[33,4])
 //# sourceMappingURL=/app.b6bd5372.map
