a7a85cfc5e46d1d2d8bb1ed1a50f0d06a84d3d38
chihiro <chanchi0707@gmail.com>
2018-06-03 14:48:56 +0900

    svgsnap入れた

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,25 @@
 /* import file */
 import * as THREE from 'three';
+import Snap from 'snapsvg';
 
 /* main */
 $(document).ready(function(){
+
+
+  /// svg animation
+  var $circle = Snap( '.circle1' );
+  function anim () {
+    // cx の値を 0 にリセット
+    $circle.attr( { cx: 0 } );
+    // cx の値を 3 秒かけて 500 にアニメーションする
+    $circle.animate( {
+      cx: 500
+    }, 3000, function () {
+      anim();
+    });
+  };
+  anim();
+
   //// slick.js
   $('.miyazu-slides').slick({
   	autoplay: true,
