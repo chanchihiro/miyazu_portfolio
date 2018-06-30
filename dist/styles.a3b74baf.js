dfde13334f975bc6856be1b99c2b66c566160b16
chihiro <chanchi0707@gmail.com>
2018-07-01 02:32:51 +0900

    矢印を直した

--- dist/styles.a3b74baf.js
+++ dist/styles.a3b74baf.js
@@ -174,7 +174,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./reset.css":19,"./variables.css":20,"./lib/slick.css":24,"./lib/slick-theme.css":25,"./slider.css":21,"./../images/arrow2.png":22,"./../images/arrow.png":23,"./../images/slide1.jpg":12,"./../images/slide1.png":13,"_css_loader":17}],90:[function(require,module,exports) {
+},{"./reset.css":19,"./variables.css":20,"./lib/slick.css":24,"./lib/slick-theme.css":25,"./slider.css":21,"./../images/arrow2.png":22,"./../images/arrow.png":23,"./../images/slide1.jpg":12,"./../images/slide1.png":13,"_css_loader":17}],96:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -343,7 +343,7 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}],91:[function(require,module,exports) {
+},{}],97:[function(require,module,exports) {
 var getBundleURL = require('./bundle-url').getBundleURL;
 
 function loadBundlesLazy(bundles) {
@@ -425,6 +425,6 @@ LazyPromise.prototype.catch = function (onError) {
   return this.promise.catch(onError);
 };
 },{"./bundle-url":34}],0:[function(require,module,exports) {
-var b=require(91);b.load([["ajax-loader.5951cde3.gif",28],["slick.29d4c64f.eot",29],["slick.526dd301.woff",30],["slick.efb232c4.ttf",31],["slick.fcb6734e.svg",32],["arrow2.e54e5543.png",22],["arrow.2b8fda1f.png",23],["slide1.5ec8952e.jpg",12],["slide1.66ad2b13.png",13]]);
-},{}]},{},[90,0])
+var b=require(97);b.load([["ajax-loader.5951cde3.gif",28],["slick.29d4c64f.eot",29],["slick.526dd301.woff",30],["slick.efb232c4.ttf",31],["slick.fcb6734e.svg",32],["arrow2.e54e5543.png",22],["arrow.2b8fda1f.png",23],["slide1.5ec8952e.jpg",12],["slide1.66ad2b13.png",13]]);
+},{}]},{},[96,0])
 //# sourceMappingURL=/styles.a3b74baf.map
