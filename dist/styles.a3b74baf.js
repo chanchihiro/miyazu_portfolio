2cf45d2b7fef6c37142f425da27ea125a01571bc
chihiro <chanchi0707@gmail.com>
2018-07-15 14:38:27 +0900

    workのタイトル消した

--- dist/styles.a3b74baf.js
+++ dist/styles.a3b74baf.js
@@ -180,7 +180,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./reset.css":30,"./variables.css":31,"./lib/slick.css":37,"./lib/slick-theme.css":38,"./slider.css":32,"./fakeLoader.css":33,"./../images/arrow2.png":34,"./../images/arrow.png":35,"./../images/slide1.jpg":13,"./../images/slide2.jpg":14,"./../images/slide1.png":36,"_css_loader":25}],108:[function(require,module,exports) {
+},{"./reset.css":30,"./variables.css":31,"./lib/slick.css":37,"./lib/slick-theme.css":38,"./slider.css":32,"./fakeLoader.css":33,"./../images/arrow2.png":34,"./../images/arrow.png":35,"./../images/slide1.jpg":13,"./../images/slide2.jpg":14,"./../images/slide1.png":36,"_css_loader":25}],116:[function(require,module,exports) {
 
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -349,7 +349,7 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}],109:[function(require,module,exports) {
+},{}],117:[function(require,module,exports) {
 var getBundleURL = require('./bundle-url').getBundleURL;
 
 function loadBundlesLazy(bundles) {
@@ -431,6 +431,6 @@ LazyPromise.prototype.catch = function (onError) {
   return this.promise.catch(onError);
 };
 },{"./bundle-url":45}],0:[function(require,module,exports) {
-var b=require(109);b.load([["ajax-loader.5951cde3.gif",40],["slick.29d4c64f.eot",41],["slick.526dd301.woff",42],["slick.efb232c4.ttf",43],["slick.fcb6734e.svg",44],["arrow2.e54e5543.png",34],["arrow.2b8fda1f.png",35],["slide1.5ec8952e.jpg",13],["slide2.ff5d5d0e.jpg",14],["slide1.66ad2b13.png",36]]);
-},{}]},{},[108,0])
+var b=require(117);b.load([["ajax-loader.5951cde3.gif",40],["slick.29d4c64f.eot",41],["slick.526dd301.woff",42],["slick.efb232c4.ttf",43],["slick.fcb6734e.svg",44],["arrow2.e54e5543.png",34],["arrow.2b8fda1f.png",35],["slide1.5ec8952e.jpg",13],["slide2.ff5d5d0e.jpg",14],["slide1.66ad2b13.png",36]]);
+},{}]},{},[116,0])
 //# sourceMappingURL=/styles.a3b74baf.map
